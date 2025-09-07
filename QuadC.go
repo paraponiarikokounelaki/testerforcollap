68a46734acca9009c422a1099a15e72a7825b3c3
empathymath <empamath@hotmail.com>
2025-09-07 08:20:22 +0300

    by me

---
+++ QuadC.go
@@ -0,0 +1,30 @@
+package rectangle
+
+import "fmt"
+
+func QuadC(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for row := 0; row < y; row++ {
+		for col := 0; col < x; col++ {
+			if row == 0 && col == x-1 {
+				fmt.Print("A")
+			} else if row == 0 && col == 0 {
+				fmt.Print("A")
+			} else if row == y-1 && col == 0 {
+				fmt.Print("C")
+			} else if row == y-1 && col == x-1 {
+				fmt.Print("C")
+			} else if row == 0 || row == y-1 {
+				fmt.Print("B")
+			} else if col == 0 || col == x-1 {
+				fmt.Print("B")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
---
+++ quad/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "rectangle"
+
+func main() {
+	rectangle.QuadA(5, 3)
+	rectangle.QuadB(5, 3)
+	rectangle.QuadC(5, 3)
+	rectangle.QuadD(5, 3)
+	rectangle.QuadE(5, 3)
+}
